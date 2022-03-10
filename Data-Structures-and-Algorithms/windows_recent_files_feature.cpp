4f1038405942da979f0ef5b243458f711ba74aa9
jnyfah <jnyfaah@gmail.com>
2022-03-10 20:06:33 +0100

    restructured Windows recent files feature

--- Data-Structures-and-Algorithms/windows_recent_files_feature.cpp
+++ Data-Structures-and-Algorithms/windows_recent_files_feature.cpp
@@ -1,39 +1,34 @@
-#include <iostream>
-#include <deque>
 
-
-/*
+/**
 ********************
-Windows Recent Files feature is an implementation of a Stack but with an added functionality
-
-You push items to the stack (strings in this implementation) and it operates on a FILO or LIFO
-basis for the pop operation
-
-As an added feature, when an item that already exists is pushed into the stack,
-it becomes the most recent item (last item) and the stack is rearranged accordingly. 
-
-The stack has a size that can be manipulated by the user.
-and it implements these functions:
-
-
-getMostRecentItem();
-pushItem();
-
-For this implementation, I will use std::deque 
+*	Windows Recent Files feature is an implementation of a Stack but with an added functionality
+*	You push items to the stack (strings in this implementation) and it operates on a FILO or LIFO
+*	basis for the pop operation
+*	As an added feature, when an item that already exists is pushed into the stack,
+*	it becomes the most recent item (last item) and the stack is rearranged accordingly. 
+*	The stack has a size that can be manipulated by the user and it implements these functions
+*
+*	getMostRecentItem();
+*	pushItem();
+*
+*	For this implementation, I will use std::deque 
 **********************************
 */
 
-class RecentStack
-{
+
+#include <iostream>
+#include <deque>
+#include <string_view>
+
+class RecentStack {
     int count;
     public:
-        std::deque<std::string> recent_stack;
+        std::deque<std::string> recentStack;
         int size;
         
         
         //Default Constructor
-        RecentStack() 
-        { 
+        RecentStack() { 
             size = 5; 
             count = size;
         }
@@ -43,39 +38,33 @@ class RecentStack
 
 
         //Returns the most recent element in the Recent Stack
-        std::string getMostRecentItem()
-        {
-            return recent_stack.back();
+        std::string getMostRecentItem() const {
+            return recentStack.back();
         }
 
 
         //Adds element to the Recent Stack
-        void pushItem( std::string item)
-        {
+        void pushItem( std::string item) {
             //search for match
-            for( auto it = recent_stack.begin(); it != recent_stack.end(); ++it)
-            {
-                if(*it == item)
-                {
+            for( auto it = recentStack.begin(); it != recentStack.end(); ++it) {
+                if(*it == item) {
                     std::cout << "Item Exists! It is added to the Front\n";
-                    recent_stack.push_back(item);
-                    recent_stack.erase(it);
+                    recentStack.push_back(item);
+                    recentStack.erase(it);
 
                     return;
                 }
             }
 
             //No match
-            if(count == -1) //RecentStack is full
-            {
-                recent_stack.push_back(item);
-                std::cout << "Oops " << recent_stack.front() << " fell off\n";
-                recent_stack.pop_front();
+            if(count == -1) { //RecentStack is full
+                recentStack.push_back(item);
+                std::cout << "Oops " << recentStack.front() << " fell off\n";
+                recentStack.pop_front();
 
-            }
-            else //push as normal and decrement counter
-            {
-                recent_stack.push_back(item);
+            } else {                            
+				//push as normal and decrement counter
+                recentStack.push_back(item);
                 --count;
             }
             
@@ -84,33 +73,26 @@ class RecentStack
 
 
         //Utility Function to print Recent Stack
-        void printRecentList()
-        {
-            for(auto i = recent_stack.end(); i != recent_stack.begin(); --i )
-            {
+        void printRecentList() {
+            for(auto i = recentStack.rbegin(); i != recentStack.rend(); ++i ) {
                 std::cout << *i << "\n";
             }
         }
 
-
-
 };
 
 
-int main()
-{
+int main() {
     int option;
-    RecentStack r_stack(5);
+    RecentStack rStack(5);
     
-
-    do
-    {
-        std::cout << "What operation do you want to perform? " << std::endl;
-        std::cout << "0. Exit" << std::endl; 
-        std::cout << "1. Add items to the stack" << std::endl;  
-        std::cout << "2. View most Recent Item" << std::endl; 
-        std::cout << "3. Print Recent List" << std::endl;   
-        std::cout << "4. Clear the Screen" << std::endl <<std::endl;  
+    do {
+        std::cout << "What operation do you want to perform? \n";
+        std::cout << "0. Exit \n";
+        std::cout << "1. Add items to the stack \n";  
+        std::cout << "2. View most Recent Item \n";
+        std::cout << "3. Print Recent List \n";  
+        std::cout << "4. Clear the Screen \n" <<std::endl;  
 
 
         std::cout << "Option: ";
@@ -118,8 +100,7 @@ int main()
 
         std::string str;
 
-        switch (option)
-        {
+        switch (option){
             case 0:
                 break;
             
@@ -127,20 +108,20 @@ int main()
                 std::cout << "Input String to Add to stack: ";
                 std::cin >> str;
 
-                r_stack.pushItem(str);
+                rStack.pushItem(str);
                 std::cout << "ADDED!\n";
 
                 break;
 
             case 2:
-                std::cout << "The Most Recent Item is: " << r_stack.getMostRecentItem();
+                std::cout << "The Most Recent Item is: " << rStack.getMostRecentItem();
                 std::cout << std::endl;
 
                 break;
 
             case 3:
                 std::cout << "The Recent Stack is Composed of: " << std::endl;
-                r_stack.printRecentList();
+                rStack.printRecentList();
 
                 std::cout << "\n\n\n";
 
@@ -154,16 +135,9 @@ int main()
                 std::cout << "Enter proper option number" << std::endl;
                 break;
 
-
-
         }  
   
     } while (option != 0);
-
-
-
-
-
 
     return 0;
 }
