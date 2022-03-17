dd7e1d29932fd22fdb20eac36c753380cdd23995
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-03-17 09:49:42 +0100

    prep to solve the bulb switcher LC question

---
+++ Data-Structures-and-Algorithms/bulb_switcher.cpp
@@ -0,0 +1,35 @@
+/*
+*
+*   Bulb Switcher
+*   Description
+*
+*   There is a room with n bulbs, numbered from 1 to n, arranged in a row from left to right. 
+*   Initially, all the bulbs are turned off.
+*
+*   At moment k (for k from 0 to n - 1), we turn on the light[k] bulb. 
+*   A bulb change color to blue only if it is on and all the previous bulbs (to the left) are turned on too.
+*
+*   Return the number of moments in which all turned on bulbs are blue.
+*
+*   Constraints:
+*
+*   n == light.length
+*   1 <= n <= 5 * 10^4
+*   light is a permutation of  [1, 2, ..., n]
+*
+*
+*
+*/
+
+
+#include <iostream>
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
