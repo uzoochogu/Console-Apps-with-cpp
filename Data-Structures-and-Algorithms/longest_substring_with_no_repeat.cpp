96a4aa42ca8227c36bc488675d158df4adb0396e
Uzochukwu Ochogu <47127185+uzoochogu@users.noreply.github.com>
2022-04-09 22:15:41 +0100

    Added an alternative solution using brute force

--- Data-Structures-and-Algorithms/longest_substring_with_no_repeat.cpp
+++ Data-Structures-and-Algorithms/longest_substring_with_no_repeat.cpp
@@ -83,6 +83,37 @@ public:
 
         
     }
+
+    //Brute force approach
+    int lengthOfLongestSubstring2(string s) {
+        int n = s.length();
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (checkRepetition(s, i, j)) {
+                    res = max(res, j - i + 1);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    bool checkRepetition(string& s, int start, int end) {
+        vector<int> chars(128);
+
+        for (int i = start; i <= end; i++) {
+            char c = s[i];
+            chars[c]++;
+            if (chars[c] > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 };
 
 
