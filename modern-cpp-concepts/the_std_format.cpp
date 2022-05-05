11142c2b0b289148ed6f051d1455160cba9423dc
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-05-05 04:32:03 +0100

    compiled first c++20 program, used msvc

--- modern-cpp-concepts/the_std_format.cpp
+++ modern-cpp-concepts/the_std_format.cpp
@@ -1,6 +1,15 @@
 //Testing modules and std::format
-import <iostream>;
-import <format>;
+//import <iostream>;  //I'll use these when they are fully supported
+//import <format>;
+
+import std.core;  
+
+/*
+The above header file gives me experimental support for <iostream> and <format> modules
+I needed to use the following flags and the msvc 14.31
+'cl /std:c++20 /experimental:module /EHsc /MD the_std_format.cpp'
+*/
+
 
 
 int main()
