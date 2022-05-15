f64b7144ae282023d805406fd9d27e7aacfc3c17
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-05-15 20:43:02 +0100

    added code to handle std::format exceptions

--- modern-cpp-concepts/the_std_format.cpp
+++ modern-cpp-concepts/the_std_format.cpp
@@ -26,7 +26,30 @@ int main()
 
     //using the std::format
 
-    std::cout << std::format("Pond diameter required for {} fish is {} feet.\n", fish_count, pond_diameter);  
+    std::cout << std::format("Pond diameter required for {} fish is {:.3} feet.\n", fish_count, pond_diameter);   //15.5
+
+
+    // Default alignment: right for numbers, left otherwise
+    std::cout << std::format("{:7}|{:7}|{:7}|{:7}|{:7}\n", 1, -.2, "str", 'c', true);
+    // Left and right alignment + custom fill character
+    std::cout << std::format("{:*<7}|{:*<7}|{:*>7}|{:*>7}|{:*>7}\n", 1,-.2,"str",'c',true);
+    // Centered alignment + 0 formatting option for numbers
+    std::cout << std::format("{:^07}|{:^07}|{:^7}|{:^7}|{:^7}\n", 1, -.2, "str", 'c', true);
+
+
+
+    //to catch std::format exceptions
+    try
+    {
+        std::cout << std::format("Pond diameter required for {:.2} fish is {:.2} feet.\n",
+        fish_count, pond_diameter);
+    }
+    catch (const std::format_error& error)
+    {
+        std::cout << error.what(); // Outputs "precision not allowed for this argument type"
+    }
+
+
 
     return 0;
 }
