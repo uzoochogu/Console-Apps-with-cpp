394da58c141aeacd15dff8aab23ce2973c942639
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-05-15 23:00:22 +0100

    demoed format specifiers and arg indexes

--- modern-cpp-concepts/the_std_format.cpp
+++ modern-cpp-concepts/the_std_format.cpp
@@ -25,10 +25,15 @@ int main()
 
 
     //using the std::format
+    std::cout << std::format("\n\n{:^50}\n","Using the std::format:");
 
     std::cout << std::format("Pond diameter required for {} fish is {:.3} feet.\n", fish_count, pond_diameter);   //15.5
 
+    
+    
+    //[[fill]align][sign][#][0][width][.precision][type]
 
+    // The width, alignment, fill, and 0 formatting options of std::format()
     // Default alignment: right for numbers, left otherwise
     std::cout << std::format("{:7}|{:7}|{:7}|{:7}|{:7}\n", 1, -.2, "str", 'c', true);
     // Left and right alignment + custom fill character
@@ -36,10 +41,26 @@ int main()
     // Centered alignment + 0 formatting option for numbers
     std::cout << std::format("{:^07}|{:^07}|{:^7}|{:^7}|{:^7}\n", 1, -.2, "str", 'c', true);
 
+    
+    //Formatting numbers
+    std::cout << std::format("\n\n{:^50}\n","Formatting numbers:");
 
+    const double pi = std::numbers::pi;
+    std::cout << std::format("Default: {:.2}, fixed: {:.2f}, scientific: {:.2e}, "
+                 "general: {:.2g}\n", pi, pi, pi, pi);
+    std::cout << std::format("Default: {}, binary: {:b}, hex.: {:x}\n", 314, 314, 314);
+    std::cout << std::format("Default: {}, decimal: {:d}, hex.: {:x}\n", 'c', 'c', 'c');
+    std::cout << std::format("Alternative hex.: {:#x}, binary: {:#b}, HEX.: {:#X}\n",
+    314, 314, 314);
+    std::cout << std::format("Forced sign: {:+}, space sign: {: }\n", 314, 314);
+    std::cout << std::format("All together: {:*<+10.4f}, {:+#09x}\n", pi, 314);
+
+    //Argument Indexing the std::format function
+    std::cout << std::format("\n\n{:<50}\n","Argument Indexing the std::format function:");
+    std::cout << std::format("Default: {0}, binary: {0:b}, hex.: {0:x}\n", 314);
 
     //to catch std::format exceptions
-    try
+    /* try
     {
         std::cout << std::format("Pond diameter required for {:.2} fish is {:.2} feet.\n",
         fish_count, pond_diameter);
@@ -47,7 +68,7 @@ int main()
     catch (const std::format_error& error)
     {
         std::cout << error.what(); // Outputs "precision not allowed for this argument type"
-    }
+    } */
 
 
 
