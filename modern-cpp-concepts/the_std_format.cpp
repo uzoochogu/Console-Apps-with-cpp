af3d524cfd381a803cd99e4580d38feab9143c82
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-05-19 07:16:07 +0100

    explored range of fundamental types with <limits>

--- modern-cpp-concepts/the_std_format.cpp
+++ modern-cpp-concepts/the_std_format.cpp
@@ -51,7 +51,7 @@ int main()
     std::cout << std::format("Default: {}, binary: {:b}, hex.: {:x}\n", 314, 314, 314);
     std::cout << std::format("Default: {}, decimal: {:d}, hex.: {:x}\n", 'c', 'c', 'c');
     std::cout << std::format("Alternative hex.: {:#x}, binary: {:#b}, HEX.: {:#X}\n",
-    314, 314, 314);
+                            314, 314, 314);
     std::cout << std::format("Forced sign: {:+}, space sign: {: }\n", 314, 314);
     std::cout << std::format("All together: {:*<+10.4f}, {:+#09x}\n", pi, 314);
 
@@ -71,6 +71,31 @@ int main()
     } */
 
 
+
+    //Now we would use it to display the limit of some fundamental types
+    //Include the <limits> module
+
+    std::cout << std::format("The range for type short is from {} to {}\n", 
+                              std::numeric_limits<short>::min(), 
+                              std::numeric_limits<short>::max())
+              << std::format("The range for type unsigned int is from {} to {}\n",
+                              std::numeric_limits<unsigned int>::min(),
+                              std::numeric_limits<unsigned int>::max())
+              << std::format("The range for type long is from {} to {}\n",
+                              std::numeric_limits<long>::min(), 
+                              std::numeric_limits<long>::max())
+              << std::format("The positive range for type float is from {} to {}\n",
+                              std::numeric_limits<float>::min(), 
+                              std::numeric_limits<float>::max())
+              << std::format("The full range for type float is from {} to {}\n",
+                              std::numeric_limits<float>::lowest(),
+                              std::numeric_limits<float>::max())
+              << std::format("The positive range for type double is from {} to {}\n",
+                              std::numeric_limits<double>::min(),
+                              std::numeric_limits<double>::max())
+              << std::format("The positive range for type long double is from {} to {}\n",
+                              std::numeric_limits<long double>::min(),
+                              std::numeric_limits<long double>::max());
 
     return 0;
 }
