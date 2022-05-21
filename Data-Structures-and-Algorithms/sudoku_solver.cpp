2eff5bad2df783c5aa8cda42d147871920fd800d
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-05-22 00:29:31 +0100

    setup for challenge

---
+++ Data-Structures-and-Algorithms/sudoku_solver.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+#include <bitset>
+#include <vector>
+
+class Solution
+{
+public:
+    void solveSudoku(std::vector<std::vector<char>> &board) const noexcept
+    {
+        /*
+        Note: input is guaranteed to be a valid board using only '1'-'9' and '.' with a uniquesolution.
+        Solution should modify board, not return a new one.
+        */
+
+
+    }
+
+};
+int main()
+{
+
+    return 0;
+}
