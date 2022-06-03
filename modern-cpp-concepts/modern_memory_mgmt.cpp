cf933bb8796c1e5ed68e11388a44fc17304cba07
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-06-03 06:51:25 +0100

    ⚙️ setup for modern memory mgmt concepts in c++

---
+++ modern-cpp-concepts/modern_memory_mgmt.cpp
@@ -0,0 +1,16 @@
+import std.core;     //We're using this temporarily
+/*
+* The above header file gives me experimental support for <iostream> and <format> modules
+* I needed to use the following flags and the msvc 14.31
+* 'cl /std:c++20 /experimental:module /EHsc /MD constexpr_keyword.cpp'
+*
+*
+* We will make use of shared pointers and unique pointers 
+*
+*
+*/
+
+int main()
+{
+
+}
