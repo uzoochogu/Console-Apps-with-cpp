08841fd565af52b048049b005f08345e05ddf3e1
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-06-03 22:58:46 +0100

    demonstrated exceptions 🛠️

--- modern-cpp-concepts/modern_memory_mgmt.cpp
+++ modern-cpp-concepts/modern_memory_mgmt.cpp
@@ -1,16 +1,54 @@
 import std.core;     //We're using this temporarily
 /*
-* The above header file gives me experimental support for <iostream> and <format> modules
+* The above header file gives me experimental support for <iostream> and <memory> modules
 * I needed to use the following flags and the msvc 14.31
-* 'cl /std:c++20 /experimental:module /EHsc /MD constexpr_keyword.cpp'
+* 'cl /std:c++20 /experimental:module /EHsc /MD modern_memory_mgmt.cpp'
 *
 *
-* We will make use of shared pointers and unique pointers 
+* We will make use of:
+* std::unique_ptr   -> Non-Copyable, just movable
+* std::shared_ptr
+* std::weak_ptr 
 *
 *
 */
 
+struct OwningInt
+{
+    OwningInt()
+        : mPtr(new int)
+    {
+
+    }
+
+    ~OwningInt()
+    {
+        std::cout << "dtor" << std::endl;
+        delete mPtr;
+    }
+
+    int *mPtr;
+};
+
+void f(const OwningInt &arg)
+{
+    try
+    {
+        OwningInt someInt;
+        throw std::exception("My Exception");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+}
+
 int main()
 {
+    std::unique_ptr<int> pi(new int(5));
 
+    f(OwningInt()); //Calls destructor after throwing an the exception
+    return 0;
 }
