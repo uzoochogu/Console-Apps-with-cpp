5b36db2fe0f85cc78ad26ed07270610e362a8301
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-06-30 01:12:14 +0100

    🧹removed string bug, annotated code📄

--- Data-Structures-and-Algorithms/gridTraveller_Challenge.cpp
+++ Data-Structures-and-Algorithms/gridTraveller_Challenge.cpp
@@ -3,18 +3,30 @@
 #include <unordered_map>
 #include <string>
 
-long gridTraveller(long row, long column, std::unordered_map<std::string, long>& memo)
+/*
+You can only move right and downwards. 
+How many possible ways to move from point A to B given a particular grid dimension.
+ _ _ _ _ _
+|A|_|_|_|_|
+|_|_|_|_|_|
+|_|_|_|_|B|
+
+Solution
+This was solved using a recursive function visualized as a tree.
+An optimal solution was obtained by memoization using C++ unordered_map.
+*/
+
+//Computes the number of possible ways to move down the grid from top left to bottom right
+long long gridTraveller(long long row, long long column, std::unordered_map<std::string, long long>& memo)
 {
     //constructing the memo object
-    std::string key = row + "," + column; 
-    std::string alt_key = column + "," + row;
+    std::string key = std::to_string(row) + "," + std::to_string(column); 
+    std::string alt_key = std::to_string(column) + "," + std::to_string(row);
     
     //searching
-    auto search = memo.find(key);
-    auto alt_search = memo.find(alt_key);
-    if(search != memo.end())
+    if(memo.find(key) != memo.end())
         return memo[key];
-    else if (alt_search != memo.end())
+    else if (memo.find(alt_key) != memo.end())
         return memo[alt_key];
 
     //other base case
@@ -29,9 +41,10 @@ long gridTraveller(long row, long column, std::unordered_map<std::string, long>&
 
 int main()
 {
-    std::unordered_map<std::string, long> memo;
+    std::unordered_map<std::string, long long> memo;
     std::cout << gridTraveller(2,3, memo) << std::endl;
-    std::cout << gridTraveller(4,8, memo)<< std::endl;
+    std::cout << gridTraveller(3,2, memo) << std::endl;
+    std::cout << gridTraveller(3,3, memo)<< std::endl;
     std::cout << gridTraveller(18,18, memo)<< std::endl;
     std::cout <<gridTraveller(28,28, memo) << std::endl;
 
