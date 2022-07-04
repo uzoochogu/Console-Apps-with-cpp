0def0a4851b4f91b0eaaccf5034a1cbe91f80019
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-07-04 23:57:10 +0100

    🔧fixed bugs, checked for matching braces

--- Data-Structures-and-Algorithms/checkForBalancedParentheses.cpp
+++ Data-Structures-and-Algorithms/checkForBalancedParentheses.cpp
@@ -12,13 +12,13 @@ bool isParenthesesBalanced(std::string exp)
   std::stack<char> braces;
   for(unsigned int i = 0; i < exp.size(); i++)
   {
-    if(exp[i] == "{" || exp[i] == "(" || exp[i] == "[")
+    if(exp[i] == '{' || exp[i] == '(' || exp[i] == '[')
     {
       braces.push(exp[i]);
     }
-    else if(exp[i] == "}" || exp[i] == ")" || exp[i] == "]")
+    else if(exp[i] == '}' || exp[i] == ')' || exp[i] == ']')
     {
-      if(braces.empty() || braces.top() != exp[i])
+      if(braces.empty() || (braces.top() == '{') && (exp[i] != '}') || (braces.top() == '(') && (exp[i] != ')')|| (braces.top() == '[') && (exp[i] != ']'))  //check if they don't match
       {
         return false;
       }
@@ -32,11 +32,11 @@ bool isParenthesesBalanced(std::string exp)
 int main()
 {
   std::cout << "Check for Balanced Parentheses:\n";
-  std::cout << "(A+B): " << isParenthesesBalanced("(A+B)") << std::endl;                                //true
-  std::cout << "{(A + B) + (C + D)}: " << isParenthesesBalanced("{(A + B) + (C + D)}: ") << std::endl;  //true
-  std::cout << "{(X + Y) * (Z): " << isParenthesesBalanced("{(X + Y) * (Z): ") << std::endl;            //false
-  std::cout << "[2 * 3] + (A)" << isParenthesesBalanced("[2 * 3] + (A)") << std::endl;                  //false
-  std::cout << "{A + Z): " << isParenthesesBalanced("{A + Z): ") << std::endl;                          //false
+  std::cout << "(A+B):               " << isParenthesesBalanced("(A+B)") << std::endl;                                //true
+  std::cout << "{(A + B) + (C + D)}: " << isParenthesesBalanced("{(A + B) + (C + D)}") << std::endl;                  //true
+  std::cout << "{(X + Y) * (Z):      " << isParenthesesBalanced("{(X + Y) * (Z)") << std::endl;                       //false
+  std::cout << "[2 * 3] + (A:        " << isParenthesesBalanced("[2 * 3] + (A") << std::endl;                         //false
+  std::cout << "{A + Z):             " << isParenthesesBalanced("{A + Z)") << std::endl;                              //false
   
   return 0;  
 }
