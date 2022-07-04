d55abeb299fd064ed79177076a876730db2a5c67
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-07-04 23:29:46 +0100

    fixed bugs, passed memo by reference

--- Data-Structures-and-Algorithms/canSum_problem.cpp
+++ Data-Structures-and-Algorithms/canSum_problem.cpp
@@ -8,7 +8,7 @@ The numbers can be repeated and combined in any way possible.
 #include <string>
 #include <vector>
 
-bool canSum(int targetSum, std::vector<int> numbers, std::unordered_map<int,bool> memo)
+bool canSum(int targetSum, std::vector<int> numbers, std::unordered_map<int,bool>& memo)
 {
   int remainder{0};
   if(memo.find(targetSum) != memo.end()) return memo[targetSum];
@@ -18,7 +18,7 @@ bool canSum(int targetSum, std::vector<int> numbers, std::unordered_map<int,bool
   for(auto i: numbers)
   {
     remainder = targetSum - i;
-    if (canSum(remainder, numbers))
+    if (canSum(remainder, numbers, memo))
     {
       memo[targetSum]= true;
       return true;
@@ -33,11 +33,11 @@ int main()
 {
   std::unordered_map <int, bool> memo;
   std::cout << "canSum:\n";
-  std::cout << "7 from {2, 3}: " << canSum(7, {2,3}, memo);              //true
-  std::cout << "7 from {5, 3, 4, 7}: " << canSum(7, {5, 3, 4, 7}, memo); //true
-  std::cout << "7 from {2, 4}: " << canSum(7, {2, 4}, memo);             //false
-  std::cout << "8 from {2, 3, 5}: " << canSum(8, {2, 3, 5}, memo);       //true
-  std::cout << "300 from {7, 14}: " << canSum(300, {7, 14}, memo);       //false
+  std::cout << "7 from {2, 3}: " << canSum(7, {2,3}, memo) << "\n";              //true
+  std::cout << "7 from {5, 3, 4, 7}: " << canSum(7, {5, 3, 4, 7}, memo) << "\n"; //true
+  std::cout << "7 from {2, 4}: " << canSum(7, {2, 4}, memo) << "\n";             //false
+  std::cout << "8 from {2, 3, 5}: " << canSum(8, {2, 3, 5}, memo) << "\n";       //true
+  std::cout << "300 from {7, 14}: " << canSum(300, {7, 14}, memo) << "\n";       //false
   
   return 0;
 }
