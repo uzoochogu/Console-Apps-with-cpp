7387dd757d2117fea0e5e6a1ed862c7c8ae857e3
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-07-18 22:15:37 +0100

    Added std::upper_bound()

--- modern-cpp-concepts/stl_algorithms_demo.cpp
+++ modern-cpp-concepts/stl_algorithms_demo.cpp
@@ -54,7 +54,7 @@ int main()
     std::cout << (binary_search(v.begin(), v.end(), 4) ? "Found" : "Not Found ") << std::endl; //outputs Not Found
 
 
-    //std::lower_bound
+    //std::lower_bound ->std::first_greater_or_equal_to
     std::cout << "\nstd::lower_bound:\n";
 
     std::vector<int> v ={ 1, 3, 3, 5, 7};
@@ -67,11 +67,25 @@ int main()
     std::cout << (it2 != v.end () ? std::to_string (*it2) : "Not Found") << std::endl;    //outputs 5
     std::cout << (it3 != v.end () ? std::to_string (*it3) : "Not Found") << std::endl;    //outputs Not Found
 
-    std::cout << distance(v.begin(), it) << endl;  //outputs 1
+    std::cout <<"To verify that only the first equal value is printed, this is the distance: " <<distance(v.begin(), it) << endl;  //outputs 1
 
     
 
-    //std::upper_bound
+    //std::upper_bound->std::first_greater_than
+    std::cout << "\nstd::lower_bound:\n";
+
+    std::vector<int> v ={ 1, 3, 3, 5, 7};
+
+    auto it  = upper_bound (v.begin (), v.end (), 3);
+    auto it2 = upper_bound (v.begin (), v.end (), 4);
+    auto it3 = upper_bound (v.begin (), v.end (), 8);
+
+    std::cout << (it != v.end () ? std::to_string (*it) : "Not Found") << std::endl;    //outputs 5
+    std::cout << (it2 != v.end () ? std::to_string (*it2) : "Not Found") << std::endl;    //outputs 5
+    std::cout << (it3 != v.end () ? std::to_string (*it3) : "Not Found") << std::endl;    //outputs Not Found
+
+   
+
 
     
 
