fcc9bb9d0c6835f5c89df5ffe4ba281d0bbee226
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-08-08 20:20:59 +0100

    🔧Debigging and logging IsSafe()

--- Data-Structures-and-Algorithms/8_Queens_problem.cpp
+++ Data-Structures-and-Algorithms/8_Queens_problem.cpp
@@ -1,7 +1,7 @@
 /**
  * @file 8_Queens_problem.cpp
  * @author Uzo Ochogu (uzoochogu@yahoo.com)
- * @brief This program finds the possible ways that 8 Queens can be arranged on a chess board such thay no queen can attack another queen
+ * @brief This program finds the possible ways that 8 or n Queens can be arm_sized on a chess board such thay no queen can attack another queen
  * @version 0.1
  * @date 2022-08-06
  * 
@@ -20,10 +20,10 @@ class Board
 public:
     Board(int size) : m_size{size}
     {
-        for(int i = 0; i < size; i++)
+        for(int i = 0; i < m_size; i++)
         {
             pieces.emplace_back();
-            for(int j =0; j < size; j++)
+            for(int j =0; j < m_size; j++)
             {
                 pieces[i].push_back(false);
             }
@@ -32,11 +32,14 @@ public:
 
     bool isSafe(int row, int column)
     {
-        for(int i =0; i < m_size; i++)
+        //int range = m_size - 1;
+        for(int i = 0; i < m_size; i++)
         {
-            if(pieces[row + i][column] || 
-               pieces[row][column + i] || 
-               pieces[row + i][column + i])
+            //check across rows, columns, principal diagonal and secondary diagonal respectively.
+            if(pieces[(row + i) % (m_size)][column] || 
+               pieces[row][(column + i) % (m_size)] || 
+               pieces[(row + i) % (m_size)][(column + i) % (m_size)] ||
+               pieces[(row - i + m_size) % (m_size)][(column + i) % (m_size)])
             {
                 return false;
             }
@@ -65,11 +68,14 @@ public:
         return output;
     };
 
+    int size()
+    {
+        return m_size;
+    }
+
 private:
     std::vector<std::vector<bool>> pieces;
     int m_size{0};
-
-
 };
 
 /**
@@ -80,13 +86,17 @@ private:
  */
 void solve8QueensUtil(Board& board, int column)
 {
-    if(column >= 8)
-        std::cout << board.toString() << std::endl;
+    //std::cout << "solve8QueensUtil("  << "1 " << column << ") \n";
+    if(column >= board.size())
+    {
+        std::cout << board.toString() << std::endl; 
+        //std::cout << "cat";
+    }
     else
     {
         //We need to place 1 queen in this column
         //for each possible place in this column
-        for (int row = 0; row < 9; row++)
+        for (int row = 0; row < board.size(); row++)
         {
             if(board.isSafe(row, column))
             {
@@ -104,7 +114,7 @@ void solve8QueensUtil(Board& board, int column)
 }
 
 /**
- * @brief Prints the boards that satisfy the possible ways that 8 Queens can be arranged on a chess board such thay no queen can attack another queen
+ * @brief Prints the boards that satisfy the possible ways that 8 or n Queens can be arm_sized on a chess board such thay no queen can attack another queen
  * 
  * @param board 
  */
@@ -116,7 +126,40 @@ void solve8Queens(Board& board)
 
 int main()
 {
-    Board b(8);
-    //solve8Queens(b);
+    /* Board b(8);
+    std::cout << b.toString();
+
+    std::cout << b.isSafe(0, 0);
+
+    b.place(2,3);
+
+    std::cout << "\n" << b.toString() << "\n";
+
+    std::cout <<  "\n" <<  b.isSafe(2,4) << "\n";
+
+    b.remove(2,3);
+
+    std::cout << b.toString();
+
+    solve8Queens(b); */
+    int size = 8;
+    Board b(size);
+    int row = 2;
+    int column = 2;
+    int m_size = 8;
+
+
+    b.place(row,column);
+
+    for(int i = 0; i < size; i++)
+    {
+        std::cout << "Rows:       " << (row + i) % (m_size) << ", " << column << "\n";
+        std::cout << "Columns:    " << row << ", "<< (column + i) % (m_size) << "\n";
+        std::cout << "P Diagonal: " << (row + i) % (m_size) << ", "<< (column + i) % (m_size)<< "\n" ;
+        std::cout << "S Diagonal: " << (row - i + m_size) % (m_size)  << ", "<< (column + i) % (m_size)<< "\n";
+    }
+    
+
+
     return 0;
 }
