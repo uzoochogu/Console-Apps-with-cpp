de1e41ada2c6a6855830870755e39f40efc812bf
Uzochukwu Chibueze Ochogu <uzoochogu@yahoo.com>
2022-09-02 21:41:02 +0100

    🛠️formattable print func using format in c++20

---
+++ modern-cpp-concepts/print.hpp
@@ -0,0 +1,17 @@
+#ifndef PRINT_HPP
+#define PRINT_HPP
+
+#include <format>
+#include <string>
+#include <string_view>
+#include <cstdio>
+
+template<typename... Args>
+void print(const std::string_view fmt_str, Args&&... args)
+{
+    auto fmt_args{ make_format_args(args...) };
+    std::string outstr{ vformat(fmt_str, fmt_args) };
+    fputs(outstr.c_str(), stdout);
+}
+
+#endif
